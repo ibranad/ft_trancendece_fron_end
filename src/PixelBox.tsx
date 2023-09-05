c889f8b94156b01b892e89fb880e161d3f6f3abd
PØM$ <90482193+Toufa7@users.noreply.github.com>
2023-09-05 12:15:09 +0100

    Add files via upload

---
+++ src/PixelBox.tsx
@@ -0,0 +1,13 @@
+import "./PixelBox.css"
+
+function PixelBox(props : any) {
+    return (
+        <div className="login-box">
+          <div className="top-login">{props.topText}</div>
+          <div className="margins">
+            </div>
+          </div>
+    );
+}
+
+export default PixelBox
